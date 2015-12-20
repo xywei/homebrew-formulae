231f6e8277e840322f2a6a77bd8bb93728833e4c
Xiaoyu WEI <xywei@users.noreply.github.com>
2015-12-20 19:23:19 +0800

    Update libmesh.rb
    updated vtk5 path

--- libmesh.rb
+++ libmesh.rb
@@ -11,8 +11,8 @@ class Petsc < Formula
   def install
     ENV.deparallelize
     args = ["--prefix=#{prefix}"]
-    args << "--with-vtk-include=/usr/local/Cellar/vtk5/5.10.1/include/vtk-5.10"
-    args << "--with-vtk-lib=/usr/local/Cellar/vtk5/5.10.1/lib/vtk-5.10"
+    args << "--with-vtk-include=/usr/local/Cellar/vtk5/5.10.1_2/include/vtk-5.10"
+    args << "--with-vtk-lib=/usr/local/Cellar/vtk5/5.10.1_2/lib/vtk-5.10"
     system "./configure", *args
     system "make"
     system "make install"
