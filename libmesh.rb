27a11236d4a53227dcaaf10bb9b951ad517d721b
Xiaoyu WEI <xywei@users.noreply.github.com>
2015-12-20 19:56:02 +0800

    Update libmesh.rb

--- libmesh.rb
+++ libmesh.rb
@@ -4,10 +4,6 @@ class Libmesh < Formula
   url "https://github.com/libMesh/libmesh/releases/download/v0.9.5-rc1/libmesh-0.9.5-rc1.tar.gz"
   sha256 "35b5229407dd900dbe8dcc21f1cf3b18790a5c7f22de60bf02b2bcda06ddf783"
 
-  depends_on :mpi => :cc
-  depends_on 'petsc'
-  depends_on 'vtk5'
-
   def install
     ENV.deparallelize
     args = ["--prefix=#{prefix}"]
